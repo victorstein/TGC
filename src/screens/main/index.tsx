57fd49ce95493a183bb0787cc81e03a4d8af3e3c
Miguel_EspinalesEs <mee@topfloormarketing.net>
2024-02-25 16:30:05 -0600

    💄 valiadated new colors in components

--- src/screens/main/index.tsx
+++ src/screens/main/index.tsx
@@ -6,11 +6,13 @@ import { ApolloProvider } from '@apollo/client'
 import { apolloStore } from '@integrations/store/store'
 import { CustomTabBar } from './components/custom-tab-bar'
 import { TabName } from './home.types'
+import { theme } from '@tailwind'
 
 const Tab = createBottomTabNavigator()
 
 export const Main = (): JSX.Element => {
   const client = apolloStore.use.client()
+  const { colors } = theme.extend
 
   return (
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion, @typescript-eslint/no-unnecessary-type-assertion
@@ -20,8 +22,8 @@ export const Main = (): JSX.Element => {
         screenOptions={{
           headerShown: false,
           unmountOnBlur: true,
-          tabBarActiveTintColor: '#D83636',
-          tabBarInactiveTintColor: '#212121'
+          tabBarActiveTintColor: colors.primary.DEFAULT,
+          tabBarInactiveTintColor: colors.text.DEFAULT
         }}
       >
         <Tab.Screen name={TabName.HOME} component={HomeScreen} />
