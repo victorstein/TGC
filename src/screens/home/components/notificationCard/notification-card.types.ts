d615c3aa2b849e23e7678018a4190c1d07204170
victorstein <stein.hakase.vs@gmail.com>
2024-02-26 21:43:25 -0600

    🚨 Fix Linter warning

--- src/screens/home/components/notificationCard/notification-card.types.ts
+++ src/screens/home/components/notificationCard/notification-card.types.ts
@@ -1,7 +1,9 @@
+import { type TabName } from '@screens/main/home.types'
+
 export interface IComponentProps {
-    photoURL: string
-    title: string
-    date: string
-    redirect?: string
-    isRead?: boolean
+  photoURL: string
+  title: string
+  date: string
+  redirect: TabName
+  isRead?: boolean
 }
