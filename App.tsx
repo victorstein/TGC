91fd50d30d6510f5e31fca9801eb5ead56e7fe78
Carlos Lenon @andadev|@boykland <boyklanding@gmail.com>
2024-02-29 22:32:12 -0600

    🐛 fix main status bar color in prod

--- App.tsx
+++ App.tsx
@@ -8,8 +8,13 @@ import {
   initialWindowMetrics
 } from 'react-native-safe-area-context'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { theme } from '@tailwind'
 import { splashStore } from './src/screens/splash/store/store'
 import * as NativeSplashScreen from 'expo-splash-screen'
+import { StatusBar } from 'expo-status-bar'
+import { mainStore, ColorScheme } from '@screens/main/store/store'
+
+const { colors } = theme.extend
 
 NativeSplashScreen.preventAutoHideAsync().catch(() => {})
 
@@ -18,12 +23,16 @@ const App = (): JSX.Element => {
   const client = useApolloCachedClient()
   const splashLoading = splashStore.use.loading()
   const MainStack = createNativeStackNavigator()
+  const coloScheme = mainStore.use.colorScheme()
 
   return (
     <SafeAreaProvider
       initialMetrics={initialWindowMetrics}
       className='flex flex-1 bg-background dark:bg-background-dark'
     >
+      <StatusBar
+        backgroundColor={`${coloScheme === ColorScheme.Dark ? colors.background.dark : colors.background.DEFAULT}`}
+      />
       <NavigationContainer>
         <MainStack.Navigator
           screenOptions={{ headerShown: false, animation: 'fade' }}
