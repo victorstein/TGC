bfb7fe9721357f17d730e7b7fc261501bdddce3b
victorstein <stein.hakase.vs@gmail.com>
2024-03-03 10:10:59 -0600

    🐛 fixed conditional

--- src/screens/main/hooks/useMain.ts
+++ src/screens/main/hooks/useMain.ts
@@ -16,7 +16,7 @@ export const useMain = (): null => {
   const isStoreHydrated = useMainStoreHydration()
 
   useEffect(() => {
-    if (isStoreHydrated) {
+    if (!isStoreHydrated) {
       return
     }
 
