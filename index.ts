8d2df8d28ab8c09bae6bb563d22dbb1e8d09da09
victorstein <stein.hakase.vs@gmail.com>
2024-03-06 05:08:01 -0600

    🚨 Ignored ts error

--- index.ts
+++ index.ts
@@ -2,7 +2,5 @@ import { registerRootComponent } from 'expo'
 
 import App from './app'
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 registerRootComponent(App)
