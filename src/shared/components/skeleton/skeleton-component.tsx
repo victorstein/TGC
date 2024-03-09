bfb785912ac3f61be4c18f50014dab4e35ced667
victorstein <stein.hakase.vs@gmail.com>
2024-03-09 08:45:42 -0600

    ♻️ refactor loading and skeleton component

---
+++ src/shared/components/skeleton/skeleton-component.tsx
@@ -0,0 +1,37 @@
+import { Skeleton, type SkeletonProps } from '@rneui/base'
+import { ColorScheme, mainStore } from '@screens/main/store/store'
+import { theme } from '@tailwind'
+import { type ViewStyle } from 'react-native'
+
+const { colors } = theme.extend
+
+export interface ISkeletonComponentProps extends SkeletonProps {}
+
+export const SkeletonComponent = (
+  props: ISkeletonComponentProps
+): JSX.Element => {
+  const colorScheme = mainStore.use.colorScheme()
+
+  return (
+    <Skeleton
+      animation='pulse'
+      skeletonStyle={{
+        backgroundColor:
+          colorScheme === ColorScheme.Dark
+            ? colors.notification.bg.dark
+            : colors.notification.bg.DEFAULT,
+        ...(props.skeletonStyle as ViewStyle)
+      }}
+      style={{
+        opacity: 0.7,
+        borderRadius: 16,
+        backgroundColor:
+          colorScheme === ColorScheme.Dark
+            ? colors.background['search-dark']
+            : colors.background.search,
+        ...(props.style as ViewStyle)
+      }}
+      {...props}
+    />
+  )
+}
